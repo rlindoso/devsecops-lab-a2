38ff5e0b813ad17aae036a068dd78e19ca90cef6
Rafael Lindoso <rafael.lindoso@gmail.com>
2026-04-13 21:39:46 -0300

    refactor: auto-init database on startup via init-db module

--- src/init-db.js
+++ src/init-db.js
@@ -11,6 +11,12 @@ async function initDB() {
   console.log('Database initialized');
 }
 
-initDB()
-  .then(() => process.exit(0))
-  .catch((err) => { console.error(err); process.exit(1); });
+// Se executado diretamente: node src/init-db.js (Job K8s, CLI)
+if (require.main === module) {
+  initDB()
+    .then(() => process.exit(0))
+    .catch((err) => { console.error(err); process.exit(1); });
+}
+
+// Se importado: require('./init-db') retorna a função
+module.exports = initDB;
